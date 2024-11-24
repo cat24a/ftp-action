bc936218bfbe6f351bf767649a4f92b33cfec00b
cat24a <artur.2010@icloud.com>
2024-11-24 12:54:13 +0100

    add support for cloning multiple directories at once

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,13 @@
 #!/bin/sh -l
 
-lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ftp:ssl-force $INPUT_FORCESSL; set ssl:verify-certificate false; mirror $INPUT_OPTIONS --reverse --continue --dereference -x ^\.git/$ $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
+commands=""
+
+local_dirs=($INPUT_LOCALDIR)
+remote_dirs=($INPUT_REMOTEDIR)
+
+for ((i=0;i<${#local_dirs[@]};i++))
+do
+    commands="$commands mirror $INPUT_OPTIONS --reverse --continue --dereference -x ^\.git/$ ${local_dirs[$i]} ${remote_dirs[$i]};"
+done
+
+lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ftp:ssl-force $INPUT_FORCESSL; set ssl:verify-certificate false; $commands quit"
