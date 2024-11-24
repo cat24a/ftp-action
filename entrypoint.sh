a68d8574844dc0add846ec4a47c2f2bf57b8ca39
cat24a <artur.2010@icloud.com>
2024-11-24 13:44:47 +0100

    use non-bash specific array functionality

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,11 @@
 
 commands=""
 
-local_dirs=($INPUT_LOCALDIR)
-remote_dirs=($INPUT_REMOTEDIR)
-
-for ((i=0;i<${#local_dirs[@]};i++))
-do
-    commands="$commands mirror $INPUT_OPTIONS --reverse --continue --dereference -x ^\.git/$ ${local_dirs[$i]} ${remote_dirs[$i]};"
+i=1
+for local_dir in $INPUT_LOCALDIR; do
+	remote_dir=$(echo $INPUT_REMOTEDIR | cut -d' ' -f$i)
+    commands="$commands mirror $INPUT_OPTIONS --reverse --continue --dereference -x ^\.git/$ $local_dir $remote_dir;"
+	i=$((i + 1))
 done
 
 lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ftp:ssl-force $INPUT_FORCESSL; set ssl:verify-certificate false; $commands quit"
